20c4953ec348c93d1784a7b83e7e6552964fd23d
Saulo Ferreira <saulolucas.gm@gmail.com>
2022-03-23 23:27:12 -0300

    Executando teste 8

--- Execution/Stress/script_to_test.py
+++ Execution/Stress/script_to_test.py
@@ -18,11 +18,12 @@ for i in range(len):
     command = ""
     command = \
         r"C:\Users\T-Gamer\Documents\apache-jmeter-5.4.1\apache-jmeter-5.4.1\bin\jmeter " + \
-        r"-n -t .\stress-cassandra.jmx -l " + r"results\test007.jtl " + \
+        r"-n -t .\stress-cassandra.jmx -l " + r"results\test008.jtl " + \
         f"-Jname=test_{tests['id'][i]} -Jlifetime={tests['duration'][i]} " + \
         f"-Jusers={tests['users'][i]} -Jlines={tests['lines'][i]} -Jconsistency={tests['consistency'][i]}"
 
     print(" id", i, "command", command)
     bar.next()
     os.system(command)
+    time.sleep(30)
 bar.finish()
